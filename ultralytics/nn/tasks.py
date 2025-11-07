b33ca35cd4652c2c7955aea3bba526528eaf6b21
nvdat16 <nguyenvandat160105@gmail.com>
2025-11-07 08:34:28 +0700

    innit

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -637,7 +637,7 @@ def torch_safe_load(weight):
                 "ultralytics.yolo.data": "ultralytics.data",
             }
         ):  # for legacy 8.0 Classify and Pose models
-            return torch.load(file, map_location="cpu"), file  # load
+            return torch.load(file, map_location="cpu", weights_only=False), file  # load
 
     except ModuleNotFoundError as e:  # e.name is missing module name
         if e.name == "models":
